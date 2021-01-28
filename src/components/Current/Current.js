16c5f1787b83e6240f521420d3c26bd8d6f1c6b0
KarlokCheung <georgecheung07@gmail.com>
2021-01-28 18:24:22 +1100

    finish metas component and current__left component

--- src/components/Current/Current.js
+++ src/components/Current/Current.js
@@ -2,6 +2,7 @@ import { render } from "@testing-library/react";
 import React from "react";
 import "./Current.css";
 import Temperature from "../Temperature/Temperature";
+import Meta from "./components/Meta/Meta";
 import VerticalDivider from "../VerticalDivider/VerticalDivider";
 
 function Current() {
@@ -12,6 +13,11 @@ function Current() {
           <Temperature degree={24} className="temperature__top" />
         </div>
         <span className="weather--description">Clouds</span>
+        <div className="metas">
+          <Meta title={"HUMIDITY"} value={"57%"} />
+          <VerticalDivider className="verticalDivider__top" />
+          <Meta title={"HUMIDITY"} value={"57%"} />
+        </div>
       </div>
       <div className="current__right"></div>
     </div>
---
+++ src/components/Current/components/Meta/Meta.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Meta = ({ title, value }) => (
+  <div className="meta">
+    <span>{title}</span>
+    <br></br>
+    <span>{value}</span>
+  </div>
+);
+
+export default Meta;
