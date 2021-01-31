3e13950865c4a8e303237d51c5eee86c77e7446f
KarlokCheung <georgecheung07@gmail.com>
2021-01-31 14:34:13 +1100

    add module css

--- src/components/Current/Current.js
+++ src/components/Current/Current.js
@@ -1,32 +1,32 @@
 import { render } from "@testing-library/react";
 import React from "react";
-import "./Current.css";
+import style from "./Current.module.css";
 import Temperature from "../Temperature/Temperature";
 import Meta from "./components/Meta/Meta";
 import VerticalDivider from "../VerticalDivider/VerticalDivider";
 
 function Current() {
   return (
-    <div className="current">
-      <div className="current__left">
-        <div>
-          <Temperature degree={24} className="temperature__top" />
+    <div className={style.current}>
+      <div className={style.current__left}>
+        <div className={style.temperature__top}>
+          <Temperature degree={24} />
         </div>
-        <span className="weather--description">Clouds</span>
-        <div className="metas">
-          <div className="humidity">
+        <span className={style.description}>Clouds</span>
+        <div className={style.metas}>
+          <div className={style.humidity}>
             <Meta title={"HUMIDITY"} value={"57%"} />
           </div>
-          <VerticalDivider className="verticalDivider__top" />
-          <div className="wind">
+          <VerticalDivider className={style.verticalDivider__top} />
+          <div className={style.wind}>
             <Meta title={"WIND"} value={"3.6 K/M"} />
           </div>
         </div>
       </div>
-      <div className="current__right">
-        <h2 className="current__city">Melbourne</h2>
+      <div className={style.current__right}>
+        <h2 className={style.current__city}>Melbourne</h2>
       </div>
-      <div className="current__bottom"></div>
+      <div className={style.current__bottom}></div>
     </div>
   );
 }
