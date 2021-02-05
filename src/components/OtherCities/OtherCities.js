cf6f4cecfd205e526d067d8eff53a6238e27506b
KarlokCheung <georgecheung07@gmail.com>
2021-02-05 23:14:13 +1100

    use map() to improve code

--- src/components/OtherCities/OtherCities.js
+++ src/components/OtherCities/OtherCities.js
@@ -2,13 +2,28 @@ import React from "react";
 import styles from "./OtherCities.module.css";
 import City from "./components/City/City";
 
+const citys = [
+  {
+    key: 1,
+    name: "Sydney",
+    degree: "22",
+    icon: "10n",
+  },
+  { key: 2, name: "Brisbane", degree: "28", icon: "03n" },
+  { key: 3, name: "Perth", degree: "31", icon: "01d" },
+];
 const OtherCities = () => (
   <div className={styles.otherCities}>
     <h2 className={styles.header}>Other Cities</h2>
     <div>
-      <City name="Sydney" degree="22" icon="10n"></City>
-      <City name="Brisbane" degree="28" icon="03n"></City>
-      <City name="Perth" degree="31" icon="01d"></City>
+      {citys.map((city) => (
+        <City
+          key={city.key}
+          name={city.name}
+          degree={city.degree}
+          icon={city.icon}
+        ></City>
+      ))}
     </div>
   </div>
 );
