5b3701351d0f72a4f750055866dc7e24a48463b3
KarlokCheung <georgecheung07@gmail.com>
2021-02-05 23:03:58 +1100

    code improve

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -3,15 +3,51 @@ import styles from "./Forecast.module.css";
 import Weather from "./components/Weather";
 
 function Forecast() {
+  const weathers = [
+    {
+      key: 1,
+      day: "MON",
+      degree: "19",
+      icon: "11d",
+    },
+    {
+      key: 2,
+      day: "TUE",
+      degree: "15",
+      icon: "01d",
+    },
+    {
+      key: 3,
+      day: "WED",
+      degree: "11",
+      icon: "03d",
+    },
+    {
+      key: 4,
+      day: "THU",
+      degree: "7",
+      icon: "50d",
+    },
+    {
+      key: 5,
+      day: "FRI",
+      degree: "18",
+      icon: "09d",
+    },
+  ];
+
   return (
     <div className={styles.forecastWeather}>
       <h2 className={styles.header}>Forecast</h2>
       <div className={styles.weathers}>
-        <Weather day="MON" degree="19" icon="11d"></Weather>
-        <Weather day="TUE" degree="15" icon="01d"></Weather>
-        <Weather day="WED" degree="11" icon="03d"></Weather>
-        <Weather day="THU" degree="7" icon="50d"></Weather>
-        <Weather day="FRI" degree="18" icon="09d"></Weather>
+        {weathers.map((weather) => (
+          <Weather
+            key={weather.key}
+            day={weather.day}
+            degree={weather.degree}
+            icon={weather.icon}
+          ></Weather>
+        ))}
       </div>
     </div>
   );
